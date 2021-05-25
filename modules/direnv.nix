6b3f3a703f90942ac16843ca9977a09aaa321a97
Junyi Hou <junyi.yi.hou@gmail.com>
2021-05-24 21:26:41 -0700

    drop ~direnv~ workaround, fixed in upstream

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,17 +1,4 @@
 inputs: with inputs; {
-  nixpkgs.overlays = [
-    (final: prev: {
-      nix-direnv = prev.nix-direnv.overrideAttrs (oldAttrs: rec {
-        # Substitute instead of wrapping because the resulting file is getting sourced, not executed:
-        postPatch = ''
-            substituteInPlace direnvrc \
-              --replace "\''${NIX_BIN_PREFIX}" "\''${NIX_BIN_PREFIX:-${prev.nix}/bin/}" \
-              --replace "grep" "${prev.gnugrep}/bin/grep"
-          '';
-      });
-    })
-  ];
-
   programs.direnv = {
     enable = true;
     enableNixDirenvIntegration = true;
